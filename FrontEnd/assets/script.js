e5924cca3e1f09e2d2094712bbfdcba41320d690
Vayalond <Vayalond@gmail.com>
2024-08-17 21:41:43 +0200

    Correction Filtre 	modified:   FrontEnd/assets/script.js 	modified:   FrontEnd/index.html 	new file:   FrontEnd/login.html

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -11,6 +11,25 @@ function displayworks(works) {
   return resultat;
 }
 
+async function filters(idcategory) {
+  try {
+    let gallery = document.getElementById("gallery");
+    let retourBackend = await backendworks();
+    if (idcategory === 1) {
+      retourBackend = retourBackend.filter((work) => work.category.id === 1)
+    }
+    else if (idcategory === 2) {
+      retourBackend = retourBackend.filter((work) => work.category.id === 2)
+    }
+    else if (idcategory === 3) {
+      retourBackend = retourBackend.filter((work) => work.category.id === 3)
+    }
+    gallery.innerHTML = displayworks(retourBackend);
+  } catch (error) {
+    console.error("Erreur lors de la récupération coté backend :", error);
+  }
+}
+
 async function backendworks() {
   const reponse = await fetch("http://localhost:5678/api/works");
   return await reponse.json();
@@ -24,6 +43,11 @@ async function init() {
   } catch (error) {
     console.error("Erreur lors de la récupération coté backend :", error);
   }
+
+  document.querySelectorAll(".filtre").forEach((bouton, index) =>
+    bouton.addEventListener('click', () => filters(index))
+  );
 }
 
 init();
+
